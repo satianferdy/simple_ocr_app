93fb4c24bfb72baca069b1a9eae7574b5abed9f7
satianferdy <satianferdy@gmail.com>
2023-12-07 17:42:49 +0700

    chore: setup asset image

---
+++ lib/pages/intro_page.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class IntroPage extends StatelessWidget {
+  const IntroPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Center(
+        child: Column(
+          // logo
+          children: [],
+          // title app
+
+          // subtitle app
+
+          // start button
+        ),
+      ),
+    );
+  }
+}
